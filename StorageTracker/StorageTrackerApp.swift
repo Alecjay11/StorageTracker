33d387b6be2796479396478b51cb3737003169d3
Alec Newman <alecjnewman@gmail.com>
2025-03-12 13:00:01 -0400

    Initial Commit

---
+++ StorageTracker/StorageTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StorageTrackerApp.swift
+//  StorageTracker
+//
+//  Created by Alec Newman on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct StorageTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
