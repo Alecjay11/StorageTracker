00204d89db196a545d7e6032b25b249aded7c637
Alec Newman <alecjnewman@gmail.com>
2025-04-25 15:10:37 -0400

    api fix

---
+++ StorageTracker/Pages/LocationPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationPickerView.swift
+//  StorageTracker
+//
+//  Created by Alec Newman on 4/25/25.
+//
+
+import SwiftUI
+
+struct LocationPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationPickerView()
+}
